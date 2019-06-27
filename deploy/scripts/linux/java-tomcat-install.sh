21c529f0369004b7158b8c9dea942594210d734b
Arpan Balpande <arpan.balpande@yash.com>
2019-06-27 16:14:01 +0530

    changed path of deployment script

---
+++ deploy/scripts/linux/java-tomcat-install.sh
@@ -0,0 +1,10 @@
+yum install java-1.8.0
+cd /usr/local
+wget http://www-us.apache.org/dist/tomcat/tomcat-9/v9.0.21/bin/apache-tomcat-9.0.21.tar.gz
+tar -xvf apache-tomcat-9.0.21.tar.gz
+rm -f apache-tomcat-9.0.21.tar.gz
+mv apache-tomcat-9.0.21 tomcat9
+echo "export CATALINA_HOME="/usr/local/tomcat9"" >> ~/.bashrc
+source ~/.bashrc
+cd /usr/local/tomcat9/bin
+./startup.sh
