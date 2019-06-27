360efea2c6508d4743bdf4b626acb940d190df2f
Arpan Balpande <arpan.balpande22@gmail.com>
2019-06-27 23:01:46 +0530

    added JAVA_HOME

--- deploy/scripts/linux/java-tomcat-install.sh
+++ deploy/scripts/linux/java-tomcat-install.sh
@@ -5,6 +5,7 @@ tar -xvf apache-tomcat-9.0.21.tar.gz
 rm -f apache-tomcat-9.0.21.tar.gz
 mv apache-tomcat-9.0.21 tomcat9
 echo "export CATALINA_HOME="/usr/local/tomcat9"" >> ~/.bashrc
+echo "export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64/jre/bin/java"" >> ~/.bashrc
 source ~/.bashrc
 cd /usr/local/tomcat9/bin
 ./startup.sh
